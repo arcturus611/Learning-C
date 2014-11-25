520ef056dbfec9ad95e48fdb6115c2c7544db5fc
arcturus611 <swati.6.itaws@gmail.com>
2014-11-24 23:51:28 -0800

    filler

--- time_functions.c
+++ time_functions.c
@@ -1,11 +1,15 @@
 	//expertcprogramming
-	//chapter 1; exercises on time
+	//chapter Introduction; exercises on time
 	
 	#include<stdio.h>
 	#include<time.h>
 	
 	int main(int argc, char* argv[]){
-		time_t timer = time(NULL);
-		printf("Current time is %s\n", ctime(&timer));
+		time_t now = time(NULL);
+		printf("Current time is %s", ctime(&now));
+		printf("Size of time_t is %lu\n", sizeof(time_t));
+		time_t biggest = 0x7fffffffffffff;
+		printf("Time at (possibly) largest value of time_t is %s", asctime(gmtime(&biggest)));
+		printf("Difference between now and biggest time is %f seconds\n", difftime(biggest, now));
 		return 0;
 	}
