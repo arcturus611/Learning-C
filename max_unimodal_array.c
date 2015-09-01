5cd3a45cc585b6f1bf4657bd314c694693543c1e
arcturus611 <swati.6.itaws@gmail.com>
2015-09-01 14:03:13 -0700

    added two more methods

--- max_unimodal_array.c
+++ max_unimodal_array.c
@@ -7,8 +7,21 @@
 	 
 	 int operation_count;
 	 
+	 int* find_max(int* start, int* end)
+	 {	
+	    // base case
+	    if (end - start < 2)
+	    {
+	        return (*start > *end) ? start: end;
+	    }
+	    int* p = (end - start) / 3 + start;
+	    int* q = (end - start) * 2 / 3 + start;
+	    // throw out whichever third has smaller element, recurse on remainder
+	    return *p > *q ? find_max(start,q - 1) : find_max(p + 1, end);
+	 }
+	 
 	 int find_peak(int* A, int n){
-	 	operation_count++;
+
 	 	if (n==1) return A[0];
 	 	
 	 	int l1 = n/2; //length of first subarray
@@ -20,6 +33,15 @@
 	 		else return find_peak(A+l1, l2); 
 	 	}
 	 	
+	 }
+	 
+	 int find_peak2(int* A, int n){
+	 	if (n==1) return A[0];
+	 	
+	 	int l1 = n/2; //length of first subarray
+	 	int l2 = n-l1;  //length of second subarray
+	 	
+	 	return (A[l1]-A[l1-1] < 0) ? find_peak(A, l1) : find_peak(A+l1, l2) ;
 	 }
 	 
 	 int main(int argc, char* argv[]){
@@ -34,7 +56,11 @@
 	 	operation_count = 0; 
 	 	printf("Max of the array is %d\n", find_peak(A, N));
 	 	printf("The operation count is %d\n", operation_count);
-	 		
+	 	
+	 	int* x = find_max(A, A+N-1);		
+	 	printf("Max of the array (method 2) is %d\n", *x);
+	 
+	 	printf("Max of array by method 3 is %d\n", find_peak2(A, N)); 		
 	 	return 0;
 	 }
 	 
